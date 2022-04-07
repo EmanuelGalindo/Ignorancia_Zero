4910bc2bc2909caaae9bd4bba9f1ca3920de98eb
EmanuelGalindo <97630262+EmanuelGalindo@users.noreply.github.com>
2022-04-07 18:54:23 -0300

    Add files via upload
    O programa pede que o usuário informe quatro notas bimestrais e calcula a média delas.

---
+++ Aula 007 - Média de quatro notas.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que peça 4 notas bimestrais e mostre a média do aluno
+'''
+
+n1 = float(input("Informe a primeira nota: "))
+n2 = float(input("Informe a segunda nota: "))
+n3 = float(input("Informe a terceira nota: "))
+n4 = float(input("Informe a quarta nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4
+print("")
+
+print(f"A média das notas informadas é: {media}")
